d0b76f6eac333fbcb6a2b5504a1760c729f23348
Diogo562 <74183161+Diogo562@users.noreply.github.com>
2020-11-09 11:48:13 +0000

    Add files via upload

---
+++ EmpregadoModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace appW.Models
+{
+    public class EmpregadoModel
+    {
+        public int empregadoID { get; set; }
+        public string nome { get; set; }
+        public string departamento { get; set; }
+
+    }
+}
